7d411c9b1b85e0bbb9366bac7ddbf4899d564803
AliceUwase <a.uwase3@alustudent.com>
2025-02-20 02:39:58 +0200

    Modified the Header, Service section and fixed bugs in the menuIcon

--- main.js
+++ main.js
@@ -1,42 +1,37 @@
-// Toggle icon navbar
+document.addEventListener('DOMContentLoaded', () => {
+    let menuIcon = document.querySelector('#menu-icon');
+    let navbar = document.querySelector('.navbar');
 
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
+    menuIcon.onclick = () => {
+        console.log('Menu clicked!');
+        menuIcon.classList.toggle('fa-xmark');
+        navbar.classList.toggle('active');
+    }
 
-menuIcon.onclick = () => {
-    menuIcon.classList.toggle('fa-x');
-    navbar.classList.toggle('active');
+    let sections = document.querySelectorAll('section');
+    let navLinks = document.querySelectorAll('header nav a');
 
-} 
+    window.onscroll = () => {
+        sections.forEach(section => {
+            let top = window.scrollY;
+            let offset = section.offsetTop - 150;
+            let height = section.offsetHeight;
+            let id = section.getAttribute('id');
 
+            if(top >= offset && top < offset + height) {
+                navLinks.forEach(links => {
+                    links.classList.remove('active');
+                    document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+                });
+            }
+        });
 
-// Scroll section active link
+        // sticky navbar
+        let header = document.querySelector('header');
+        header.classList.toggle('sticky', window.scrollY > 100);
 
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
-
-window.onscroll = () => {
-    sections.forEach(section => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop -150;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
-
-        if(top >= offset && top < offset + height) {
-            navLinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-            });
-        };
-    });
-
-    // sticky navbar
-    let header = document.querySelector('header');
-    header.classList.toggle('sticky', window.scrollY > 100);
-
-    // remove toggle icon and navbar
-    menuIcon.classList.remove('fa-x');
-    navbar.classList.remove('active');
-};
-
-
+        // remove toggle icon and navbar when scroll
+        menuIcon.classList.remove('fa-xmark');
+        navbar.classList.remove('active');
+    };
+});
